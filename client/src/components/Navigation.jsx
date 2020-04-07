f38e06e27e8e4577a9e8d8649f1282d255896c72
leer <ladesigns90@gmail.com>
2020-04-07 03:21:57 -0400

    Cleaned up navbar. Started on readme file.

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -13,21 +13,9 @@ export default class Navigation extends Component {
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="mr-auto">
                         <Nav.Link href="/">Dashboard</Nav.Link>
-                        
-                        <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                        </NavDropdown>
-                        </Nav>
-                        <Nav>
                         <Nav.Link href="/notes/">Class Notes</Nav.Link>
-                        <Nav.Link eventKey={2} href="#memes">
-                            Dank memes
-                        </Nav.Link>
                         </Nav>
+                        
                     </Navbar.Collapse>
                 </Navbar>
                 
---
+++ readme.md
@@ -0,0 +1,22 @@
+# SEI26 Project 4: ABC123 Online School Platform
+
+A web application that education professionals can use to facilitate their courses online. 
+
+## User Story
+Technology has advanced so much that the demand for remote learning has tremedously increased. Consumers or prospective students like the convenience factor and enables them to maintain a healthy work-life balance. 
+
+Educational professionals with use of this platform are able to facilitate to their markets needs. Also they are able to archive content and dynamically change information in a matter of seconds, as oppose to supporting documents already printed out. 
+
+## Heroku App Link (VIEW APP HERE)
+Live App Link: 
+
+## Trello Project Board
+View Here: https://trello.com/b/eCqOwL5P/abc123-online-school
+
+## Wireframes 
+<img src="https://github.com/lrobert4/SEIProject2/blob/master/Wireframes-one.jpg" alt="Wireframe Images One">
+
+<img src="https://github.com/lrobert4/SEIProject2/blob/master/wireframes-two.jpg" alt="Wireframe Images Two">
+
+## ERD Diagram
+<img src="https://github.com/lrobert4/SEIProject2/blob/master/ERD_Diagram.jpg" alt="ERD Diagram">
